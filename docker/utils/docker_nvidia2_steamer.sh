1b40dbcd5c6db5078a9301e55653dfe821809a9b
carebare47 <tom_q@hotmail.co.uk>
2019-04-01 17:58:08 +0100

    updated

--- docker/utils/docker_nvidia2_steamer.sh
+++ docker/utils/docker_nvidia2_steamer.sh
@@ -93,33 +93,33 @@ RUN apt-get update && apt-get install -y \
 
 # Create the user and checkout it
 ENV STEAM_USER steam
-RUN useradd -ms /bin/bash $STEAM_USER
-RUN usermod -aG sudo $STEAM_USER
+RUN useradd -ms /bin/bash \$STEAM_USER
+RUN usermod -aG sudo \$STEAM_USER
 
 
 RUN echo steam:root | chpasswd
 RUN echo root:root | chpasswd
-USER $STEAM_USER
+USER \$STEAM_USER
 
 # Set the working directory
-ENV WD /home/$STEAM_USER
-WORKDIR $WD
+ENV WD /home/\$STEAM_USER
+WORKDIR \$WD
 
 # Create the steam directory and download the SteamCMD into it
 ENV STEAM_DIR steamcmd
-RUN mkdir -p $STEAM_DIR && \
-    curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | \
-    tar -C $STEAM_DIR -zxv
+RUN mkdir -p \$STEAM_DIR && \
+    curl -sqL \"https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz\" | \
+    tar -C \$STEAM_DIR -zxv
 
 # Install the steam cmd
-RUN $STEAM_DIR/steamcmd.sh +login anonymous +quit
+RUN \$STEAM_DIR/steamcmd.sh +login anonymous +quit
 
 # Bugfix, for default when the game server is started, it searches 
 # where the steam client is (for default search in the .steam/sdk32
 # directory
 ENV HIDDEN_DIR $WD/.steam/sdk32
-RUN mkdir -p $HIDDEN_DIR
-RUN cp $STEAM_DIR/linux32/steamclient.so $HIDDEN_DIR" >> Dockerfile
+RUN mkdir -p \$HIDDEN_DIR
+RUN cp \$STEAM_DIR/linux32/steamclient.so \$HIDDEN_DIR" >> Dockerfile
 
 docker build --tag "$docker_image-steam-nvidia2" .
 
