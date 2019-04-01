a5291745088aa4ebd9a4b2b53090d80600edadda
carebare47 <tom_q@hotmail.co.uk>
2019-04-01 19:55:16 +0100

    sad times with the steam license

--- docker/utils/docker_nvidia2_steamer2.sh
+++ docker/utils/docker_nvidia2_steamer2.sh
@@ -99,8 +99,10 @@ RUN dpkg --add-architecture i386
 RUN add-apt-repository multiverse
 RUN apt-get update
 RUN apt-get dist-upgrade -y
-RUN echo steam steam/question select \"I AGREE\" | debconf-set-selections
-RUN echo steam steam/license note '' | debconf-set-selections
+RUN echo \"steam steam/purge note\" |  debconf-set-selections
+RUN echo \"steam steam/license note\" |  debconf-set-selections
+RUN echo \"steam steam/question select I AGREE\" |  debconf-set-selections 
+
 RUN apt-get install -y steam
 
 RUN cd /home/user
